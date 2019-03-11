2f03a6b142fe1865c097c36f76fd1101c1db146b
Wadood Chaudhary <wc2595@columbia.edu>
2019-03-10 21:40:59 -0400

    Add files via upload

---
+++ TaqwaJava/taqwaOS/src/taqwaos/TaqwaOS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package taqwaos;
+
+import taqwa.ApplicationQuranTree;
+
+/**
+ *
+ * @author Wadood
+ */
+public class TaqwaOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ApplicationQuranTree();
+    }
+    
+}
