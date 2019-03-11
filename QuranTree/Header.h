53390bca989bdd3d389645bb51a4f9d705f754a0
Wadood Chaudhary <wc2595@columbia.edu>
2019-03-10 20:02:01 -0400

    Add files via upload

---
+++ QuranTree/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  AvatarManager
+//
+//  Created by Wadood Chaudhary on 4/29/15.
+//  Copyright (c) 2015 Wadood Chaudhary. All rights reserved.
+//
+
+#import <AWSCore/AWSCore.h>
+#import <AWSS3/AWSS3.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSSQS/AWSSQS.h>
+#import <AWSSNS/AWSSNS.h>
+#import <AWSCognito/AWSCognito.h>
